788a4363adaf5482140454e7ddc6932ec57041a7
O-lolade <olushesim@gmail.com>
2023-09-11 04:30:10 -0700

    0-preprocessor

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints various comptuer types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of a char
