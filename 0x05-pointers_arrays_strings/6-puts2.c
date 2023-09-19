b94054a4941ec1fd220bb13fb27b97882c98ae0e
O-lolade <olushesim@gmail.com>
2023-09-19 14:30:45 -0700

    ./9-strcpy

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,31 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - function should print only one charater out of two
- * starting with the first one
- * @str: input
- * Return: print
+ * puts2 - print every other character of a string
+ * @str: string to be printed
+ * Return: 0
  */
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i = 0;
 
-	while (*y != '\0')
+	while (str[i] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(str[0]);
+			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: dest
+ * *_strcpy - copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: copy the string pointed to by pointer 'src' to
+ * the buffer pointer to by 'dest'
+ * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
+	int z = -1;
 
-	while (*(src + 1) != '\0')
+	do
 	{
-		l++;
+		z++;
+		dest[z] = src[z];
 	}
-	for (; x < l; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
+	while (src[z] != '\0');
+
 	return (dest);
 }
