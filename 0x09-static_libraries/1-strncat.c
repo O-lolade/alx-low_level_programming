8b9cce21c799ecf31e4ef883c5b611319b888375
O-lolade <olushesim@gmail.com>
2023-10-06 07:25:55 -0700

    ./quote

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -14,16 +14,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int a;
 	int b;
-	
+
 	a = 0;
-	
+
 	while (dest[a] != '\0')
 	{
 		a++;
 	}
-	
+
 	b = 0;
-	
+
 	while (b < n && src[b] != '\0')
 	{
 		dest[a] = src[b];
@@ -31,7 +31,7 @@ char *_strncat(char *dest, char *src, int n)
 		b++;
 	}
 
-	dest[a] = '\0'
-		
+	dest[a] = '\0';
+
 	return (dest);
 }
